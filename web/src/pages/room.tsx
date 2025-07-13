a3922b21a23636ff426ed1b08fba64140bf78d49
Claudio Pereira <claudiofrancopereira@gmail.com>
2025-07-13 20:50:49 -0300

    feat: app setup, rooms listing

---
+++ web/src/pages/room.tsx
@@ -0,0 +1,27 @@
+import { Navigate, useParams } from "react-router-dom"
+
+type RoomParams = {
+    roomID: string
+
+}
+
+
+
+
+
+
+
+
+
+
+
+export function Room() {
+    const params = useParams<RoomParams>()
+
+    if (!params.roomID) {
+        return <Navigate replace to="/" />
+        
+    }
+
+    return <div>Room Details {JSON.stringify(params)}</div>
+}
